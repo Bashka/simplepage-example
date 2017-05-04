aa146a07f7be86be3315b178fc04fc5774f646fa
artur <Artur-Mamedbekov@yandex.ru>
2017-05-04 13:56:10 +0300

    Fix

--- config.php
+++ config.php
@@ -1,15 +1,12 @@
 <?php
 return [
   'plugins' => [
-    '_plugins/autoload.php',
     '_plugins/input.php',
     '_plugins/middleware.php',
     '_plugins/i18n.php',
     '_plugins/error.php',
     '_plugins/pdo.php',
-    '_plugins/acl.php',
     '_plugins/layout.php',
-    '_plugins/hook.php',
   ],
   'layout' => [
     'title' => 'SimplePage',
